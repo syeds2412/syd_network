bf78d8eabe996b40f668897c90a8ffc420dc443b
syeds2412 <s.sarfaraz2412@yahoo.com>
2022-04-28 15:15:50 +0530

    added test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def hello(name,age):
+    print(f"Hello my name is {name}, and my age is {age}")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+hello(name,age)
+
