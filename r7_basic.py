fcba205d3b9af771030cafae5c37e3d4ead52cdf
Syed Sarfaraj Ahamad <33339611+syeds2412@users.noreply.github.com>
2022-07-31 20:00:07 +0530

    Add files via upload

---
+++ r7_basic.py
@@ -0,0 +1,41 @@
+#Importing correct modules
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from netmiko.ssh_exception import NetMikoAuthenticationException
+from netmiko.ssh_exception import SSHException
+import os
+
+#Cisco device to login
+iosv_l3 = {'device_type': 'cisco_ios', 'ip': '192.168.1.17', 'username': 'cisco', 'password': 'cisco', 'secret': 'cisco'}
+
+#Below script shows ssh to the device and provide error in case error happens
+#try:
+net_connect = ConnectHandler(**iosv_l3)
+
+#except NetMikoTimeoutException:
+#	print('Device not reachable')
+#	continue
+
+#except NetMikoAuthenticationException:
+#   print('Authentication failure')
+#   continue
+
+#except SSHException:
+#    print('Make sure SSH is enabled.')
+#    continue
+
+#Will check if need for enable mode on Cisco ios 
+if not net_connect.check_enable_mode():
+	net_connect.enable()
+
+#Lines to config on device
+with open('R7.txt') as f:
+	lines = f.read().splitlines()
+
+print(lines)
+
+#Push required config to the device
+output = net_connect.send_config_set(lines)
+
+print(output)
+
